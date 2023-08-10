12a143d99ca8737c20eb4acdae25b34c646cadb6
rp-boxdev <rp.boxdev@gmail.com>
2023-08-10 13:19:05 -0400

    Clean up unused code

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// const gridButton = document.querySelector("#grid-button");
-// const cdiv = document.querySelector("#grid-container");
-// const grid = document.querySelector(".grid");
-
-// // Add 16 divs
-// for (let i = 1; i < 273; i++) {
-//   const div = document.createElement("div");
-//   div.classList.add("grid");
-//   div.addEventListener("mousedown", () => {
-//     div.style.backgroundColor = "red"; // Change to desired color on hover
-//   });
-//   div.style.cursor = "grab";
-//   cdiv.appendChild(div);
-// }
-
-// gridButton.addEventListener("click", () => {
-//   prompt("Enter a number for the grid size: ");
-// });
-
-
 const gridContainer = document.querySelector('.grid-container');
 let isDragging = false;
 
