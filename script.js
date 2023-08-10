23c9981fef802ca4aada5283e45ba01b0d617953
rp-boxdev <rp.boxdev@gmail.com>
2023-08-10 13:16:21 -0400

    Rework grid logic and functionality

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
-const cdiv = document.querySelector("#grid-container");
-const grid = document.querySelector(".grid");
+// const gridButton = document.querySelector("#grid-button");
+// const cdiv = document.querySelector("#grid-container");
+// const grid = document.querySelector(".grid");
 
-// Add 16 divs
-for (let i = 1; i < 273; i++) {
-  const div = document.createElement("div");
-  div.classList.add("grid");
-  div.addEventListener("mouseenter", () => {
-    div.style.backgroundColor = "red"; // Change to desired color on hover
-  });
-  cdiv.appendChild(div);
+// // Add 16 divs
+// for (let i = 1; i < 273; i++) {
+//   const div = document.createElement("div");
+//   div.classList.add("grid");
+//   div.addEventListener("mousedown", () => {
+//     div.style.backgroundColor = "red"; // Change to desired color on hover
+//   });
+//   div.style.cursor = "grab";
+//   cdiv.appendChild(div);
+// }
+
+// gridButton.addEventListener("click", () => {
+//   prompt("Enter a number for the grid size: ");
+// });
+
+
+const gridContainer = document.querySelector('.grid-container');
+let isDragging = false;
+
+function createGrid() {
+  for (let i = 0; i < 256; i++) {
+    const gridItem = document.createElement('div');
+    gridItem.classList.add('grid-item');
+    gridContainer.appendChild(gridItem);
+
+    gridItem.addEventListener('mousedown', () => {
+      isDragging = true;
+      gridItem.classList.add('active');
+    });
+
+    gridItem.addEventListener('mouseover', () => {
+      if (isDragging) {
+        gridItem.classList.add('active');
+      }
+    });
+  }
 }
+
+document.addEventListener('mouseup', () => {
+  isDragging = false;
+});
+
+createGrid();
