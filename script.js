410da9c7117f07b892038d7f288631808760ee57
rp-boxdev <rp.boxdev@gmail.com>
2023-08-10 13:56:30 -0400

    Add ability to select grid size

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
-const gridContainer = document.querySelector('.grid-container');
+const gridContainer = document.querySelector(".grid-container");
+const gridButton = document.getElementById("grid-button");
+const numSquaresInput = document.getElementById("numSquares");
 let isDragging = false;
 
+gridButton.addEventListener("click", createGrid);
+
 function createGrid() {
-  for (let i = 0; i < 256; i++) {
-    const gridItem = document.createElement('div');
-    gridItem.classList.add('grid-item');
+  const numSquares = parseInt(numSquaresInput.value);
+
+  if (numSquares <= 0 || numSquares > 100) {
+    alert("Please enter a valid number between 1 and 100.");
+    return;
+  }
+
+  gridContainer.innerHTML = ""; // Clear existing grid
+
+  gridContainer.style.gridTemplateColumns = `repeat(${numSquares}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${numSquares}, 1fr)`;
+
+  for (let i = 0; i < numSquares * numSquares; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("grid-item");
     gridContainer.appendChild(gridItem);
 
-    gridItem.addEventListener('mousedown', () => {
+    gridItem.addEventListener("mousedown", () => {
       isDragging = true;
-      gridItem.classList.add('active');
+      toggleActive(gridItem);
     });
 
-    gridItem.addEventListener('mouseover', () => {
+    gridItem.addEventListener("mouseover", () => {
       if (isDragging) {
-        gridItem.classList.add('active');
+        toggleActive(gridItem);
       }
     });
   }
 }
 
-document.addEventListener('mouseup', () => {
+document.addEventListener("mouseup", () => {
   isDragging = false;
 });
+
+function toggleActive(element) {
+  element.classList.toggle("active");
+}
 
 createGrid();
